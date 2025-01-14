1c96d5eb38ec11bf044a1a771f657713d5723ecc
tlamart <tlamart@xps>
2025-01-14 11:32:28 +0100

    refacto using class instead of object for bar and ball

--- main.lua
+++ main.lua
@@ -13,56 +13,53 @@ function drawRect(rectangle)
 end
 
 function love.load()
-  rectTop = createRect("fill", 300, 5, 200, 15)
-  rectBottom = createRect("line", 300, 600 - 15 - 5, 200, 15)
-  rectLeft = createRect("fill", 5, 250, 15, 200)
-  rectRight = createRect("line", 800 - 15 - 5, 250, 15, 200)
+  Object = require "classic"
+  require "bar"
+  require "ball"
+  barTop = Bar("fill", 300, 5, 200, 15)
+  barBottom = Bar("line", 300, 600 - 15 - 5, 200, 15)
+  barLeft = Bar("fill", 5, 250, 15, 200)
+  barRight = Bar("line", 800 - 15 - 5, 250, 15, 200)
   speed = 450
-  ball1 = {}
-  ball1.xStart = 395
-  ball1.yStart = 295
-  ball1.x = ball1.xStart
-  ball1.y = ball1.yStart
-  ball1.vx = -1
-  ball1.vy = 0
+  ball1 = Ball(395, 295, -1, 0)
 end
 
 function love.update(dt)
   if love.keyboard.isDown("up") then
-    rectRight.y = rectRight.y - speed * dt
+    barRight.up(barRight, dt)
   elseif love.keyboard.isDown("down") then
-    rectRight.y = rectRight.y + speed * dt
+    barRight.down(barRight, dt)
   end
   if love.keyboard.isDown("left") then
-    rectBottom.x = rectBottom.x - speed * dt
+    barBottom.left(barBottom, dt)
   elseif love.keyboard.isDown("right") then
-    rectBottom.x = rectBottom.x + speed * dt
+    barBottom.right(barBottom, dt)
   end
   if love.keyboard.isDown("z") then
-    rectLeft.y = rectLeft.y - speed * dt
+    barLeft.up(barLeft, dt)
   elseif love.keyboard.isDown("s") then
-    rectLeft.y = rectLeft.y + speed * dt
+    barLeft.down(barLeft, dt)
   end
   if love.keyboard.isDown("q") then
-    rectTop.x = rectTop.x - speed * dt
+    barTop.left(barTop, dt)
   elseif love.keyboard.isDown("d") then
-    rectTop.x = rectTop.x + speed * dt
+    barTop.right(barTop, dt)
   end
   if ball1.x < 0 or ball1.x > 800 then
     -- game over just reset ball position for now
-    ball1.x = ball1.xStart
+    ball1.x = ball1.xstart
   end
-  if (ball1.x <= 5 + 15 and ball1.y >= rectLeft.y and ball1.y <= rectLeft.y + 200) or (ball1.x >= 800 - 15 - 5  and ball1.y >= rectRight.y and ball1.y <= rectRight.y + 200) then
+  if (ball1.x <= 5 + 15 and ball1.y >= barLeft.y and ball1.y <= barLeft.y + 200) or (ball1.x >= 800 - 15 - 5  and ball1.y >= barRight.y and ball1.y <= barRight.y + 200) then
     ball1.vx = ball1.vx * (-1)
   end
-  ball1.x = ball1.x + speed / 3 * ball1.vx * dt
-  ball1.y = ball1.y + speed / 3 * ball1.vy * dt
+  ball1.x = ball1.x + speed / 2 * ball1.vx * dt
+  ball1.y = ball1.y + speed / 2 * ball1.vy * dt
 end
 
 function love.draw()
-  drawRect(rectTop)
-  drawRect(rectLeft)
-  drawRect(rectRight)
-  drawRect(rectBottom)
+  barTop.draw(barTop)
+  barLeft.draw(barLeft)
+  barRight.draw(barRight)
+  barBottom.draw(barBottom)
   love.graphics.circle("fill", ball1.x, ball1.y, 10)
 end
